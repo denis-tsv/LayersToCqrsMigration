fe9f5e576e471a5199903f0136ab1c5eb002f1c6
Denis Tsvettsikh <denis_tsvettsikh@epam.com>
2020-09-27 16:02:25 +0700

    Add product controller

---
+++ Before/Host/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace Host.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("{id}")]
+        public Task<ProductDto> Get(int id)
+        {
+            return _productService.GetAsync(id);
+        }
+    }
+}
